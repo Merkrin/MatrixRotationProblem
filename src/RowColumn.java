4264ee23f2208d2d38cb2ea8983066926c308b84
Irina Barciuc <iabarchuk@edu.hse.ru>
2023-02-27 08:34:50 +0100

    Deleted excesses

--- src/RowColumn.java
+++
@@ -1,9 +0,0 @@
-public class RowColumn {
-    public int row;
-    public int column;
-
-    public RowColumn(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-}
